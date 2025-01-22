418f3dff48fb69508ca777c5d89a8402e248801d
vinay961 <vinay100rai@gmail.com>
2025-01-22 10:06:31 +0530

    learning day-1

---
+++ myCSharpProject/myFirstApp/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first Number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Second Number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.WriteLine("What operation to perform(add,sub,mul,div)");
+        string op = Console.ReadLine();
+
+        switch (op)
+        {
+            case "add":
+                System.Console.WriteLine(num1+num2);
+                break;
+            case "sub":
+                System.Console.WriteLine(num1-num2);
+                break;
+            case "mul":
+                System.Console.WriteLine(num1*num2);
+                break;
+            case "div":
+                if(num2 != 0){
+                    System.Console.WriteLine(num1/num2);
+                }
+                else
+                {
+                    System.Console.WriteLine("Division by zero is not possible!!");
+                }
+                break;
+            default:
+                System.Console.WriteLine("Enter the correct operation!!");
+                break;
+        }
+    }
+}
---
+++ myCSharpProject/mySecondApp/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the Number Guessing Game!");
+        System.Console.WriteLine("I have selected a number between 1 and 100. Try to guess it!");
+
+        // Generate a random number between 1 to 100
+        Random random = new Random();
+        int targetNumber = random.Next(1, 101);
+
+        int attempt = 7;
+
+        while (attempt > 0)
+        {
+            System.Console.Write("Guess the number: ");
+            int input = Convert.ToInt32(System.Console.ReadLine());
+
+            if(input == targetNumber){
+                System.Console.WriteLine($"..You \"Win\" in just {8-attempt} attempts..");
+                break;
+            }    
+            else{
+                attempt --;
+                if(input > targetNumber){
+                    System.Console.WriteLine($"Gussed Number is greater, Only {attempt} attempts left!");
+                    continue;
+                }
+                else{
+                    System.Console.WriteLine($"Gussed Number is smaller, Only {attempt} attempts left!");
+                    continue;
+                }
+            }
+        }
+    }
+}
